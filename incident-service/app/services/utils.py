661cc300f3dddd9a59127fd59b594dbe75897bd2
Haqq Bassit <munagahmnh@gmail.com>
2026-03-30 12:54:43 +0000

    Implement coordinates to region mapping

---
+++ incident-service/app/services/utils.py
@@ -0,0 +1,21 @@
+import geopandas as gpd
+from shapely.geometry import Point
+
+gdf = gpd.read_file("data/ghana_regions.geojson")
+
+sindex = gdf.sindex
+
+
+def _region_from_coords(lat: float, lon: float) -> str:
+    point = Point(lon, lat)
+
+    possible_idx = list(sindex.intersection(point.bounds))
+
+    for idx in possible_idx:
+        row = gdf.iloc[idx]
+        if row.geometry.contains(point):
+            return row["shapeName"]
+
+    return "Unknown"
+
+print(_region_from_coords(5.025118, -0.218568))
